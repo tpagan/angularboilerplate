748faef8d8931372db99de2bd84353545ef49514
GRAYCON_CHI\tpagan <tpagan@travelclick.com>
2017-03-07 09:56:10 -0500

    Added all templates from Boiler Plate. Not Susr eif these are necesary.

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'ngAnimate', 
+  'ngAnimate',
+  'ngSanitize', 
   'myApp.view1',
   'myApp.view2',
   'myApp.version',
